1d40d4dff166797ed3d3b5128410e8966d32ecec
jervygu <jervygu@gmail.com>
2020-06-06 12:49:10 +0800

    Loops in Swift

---
+++ Loops in Swift.playground/Contents.swift
@@ -0,0 +1,41 @@
+import UIKit
+//
+//let arrayNumber = [1,2,3,4,5,6,7,8,0]
+//
+//for num in arrayNumber {
+//    print(num)
+//    print("new cycle")
+//}
+
+// getting sum of the arrayNumber
+//let arrayNumber = [1,2,3,4,5,6,7,8,0,10]
+//var sum = 0
+//
+//for num in arrayNumber {
+//    sum += num
+//}
+//
+//print(sum)
+
+// print 1 - 10
+//for number in 1 ... 10 {
+//    print(number)
+//}
+
+
+// print 1 to < 9 ... not including last
+//for numberB in 1 ..< 10 {
+//    print(numberB)
+//}
+
+
+// print 1 to 10 ... where numberC is even number or divisible by 2
+//for numberC in 1 ... 10  where numberC % 2 == 0{
+//    print(numberC)
+//}
+
+
+// print 1 to 10 ... where numberD is odd number or not divisible by 2
+for numberD in 1 ... 10  where numberD % 2 != 0{
+    print(numberD)
+}
